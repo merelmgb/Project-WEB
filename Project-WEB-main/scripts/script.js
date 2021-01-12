a7735bc762a9d6e159432b033c6e2f013d7e58b0
merelmgb <43814966+merelmgb@users.noreply.github.com>
2021-01-12 14:43:26 +0100

    Add files via upload

---
+++ Project-WEB-main/scripts/script.js
@@ -0,0 +1,17 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console': 0*/
+
+/*console.log('Hello World!'); */
+
+
+var deButton = document.querySelector("nav button");
+
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("toonMenu");
+}
+
